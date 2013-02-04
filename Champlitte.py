78c6d96298bb018a7acc9404ffbf85e8db5f3454
JeanFred <JeanFred@github>
2013-02-04 22:40:56 +0100

    Finish revamping Champlitte.py after code move

--- Champlitte.py
+++ Champlitte.py
@@ -1,46 +1,32 @@
 # -*- coding: latin-1 -*-
 
-"""Partnership with the Champlitte museum − handling metadata collection."""
+"""Partnership with the Champlitte museum."""
 
 __authors__ = 'User:Jean-Frédéric'
 
-import sys
-import codecs
-import csv
-import UnicodeCSV
-from Record import Record
+from uploadlibrary import metadata
 
 
-class RecordsProcessing:
+class ChamplitteRecord(metadata.MetadataRecord):
+
+    """Represent a record, with its associated metadata."""
+
+    def get_title(self):
+        """Return the title for the file."""
+        name = self.__dict__.get('JOCONDE_TITR',
+                                 self.__dict__.get('JOCONDE_DENO', ""))
+        return "%s - %s - CG70" % (name, self.INV)
+
+
+class ChamplitteMetadataCollection(metadata.MetadataCollection):
 
     """Processing a collection of Records in various ways."""
 
-    def __init__(self):
-        self.records = []
+    def handle_record(self, row):
+        """Handle a record."""
 
-    def retrieve_metadata_from_csv(self, csv_file):
-        """Retrieve metadata from the given CSV file."""
-        file_handler = codecs.open(csv_file, 'r', 'utf-8')
-        csvReader = UnicodeCSV.unicode_csv_dictreader(file_handler,
-                                                      delimiter=',')
-        try:
-            for row in csvReader:
-                joconde_field = row.pop('Actimuse::_Export_Global DMF').split('\n')
-                joconde = dict(zip(["JOCONDE_%s" % x for x in joconde_field[0::2]],
-                                    joconde_field[1::2]))
-                row.update(joconde)
-                record = Record(**row)
-                record.post_process()
-                self.records.append(record)
-        except csv.Error, e:
-            sys.exit('file %s, line %d: %s' % (self.csv_file,
-                                               csvReader.line_num, e))
-
-    def print_metadata_of_record(self, index):
-        """Print the metadata of the record.
-
-        Print the result of get_title and of to_template.
-
-        """
-        print self.records[index].get_title()
-        print self.records[index].to_template()
+        joconde_field = row.pop('Actimuse::_Export_Global DMF').split('\n')
+        joconde = dict(zip(["JOCONDE_%s" % x for x in joconde_field[0::2]],
+                            joconde_field[1::2]))
+        row.update(joconde)
+        return ChamplitteRecord(**row)
